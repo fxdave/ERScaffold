0ad843ea20f556fc2799f3395152372a2a209e60
David Judge <dbiro97@gmail.com>
2018-11-06 10:52:31 +0100

    new version

--- oldsrc/Elements/Connections/Connections.js
+++
@@ -1,9 +0,0 @@
-import OneToMany from './OneToMany'
-import OneToOne from './OneToOne'
-import ManyToMany from './ManyToMany'
-
-export {
-    OneToOne,
-    OneToMany,
-    ManyToMany
-};
--- oldsrc/Elements/Elements.js
+++
@@ -1,9 +0,0 @@
-import Entity from './Entity'
-import Property from './Property'
-import * as Connections from './Connections/Connections'
-
-export {
-    Entity,
-    Property,
-    Connections
-}
