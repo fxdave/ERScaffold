d19e38b4854a10c052790ba70a36dbd8d0b0341b
David Judge <dbiro97@gmail.com>
2019-02-20 10:53:04 +0100

    scratched git wrapper

--- src/main/wrappers/git.js
+++ src/main/wrappers/git.js
@@ -1,7 +1,86 @@
 const gitP = require('simple-git/promise');
-const git = gitP(__dirname);
+const ERSCAFFOLD_BRANCH_NAME = 'ERBranch'
+const COMMIT_MESSAGE = 'ER model has generated'
+const WORKDIR = __dirname
+const sgit = gitP(WORKDIR);
 
-git.checkIsRepo()
+sgit.checkIsRepo()
     .then(isRepo => {
 
     })
+
+
+/**
+ * Procedure: 
+ * if has ERBranch
+ *  checkout
+ * else
+ *  create and checkout
+ * rollback to first commit in the branch
+ * some modifications
+ * commit
+ * 
+ */
+const git = {}
+
+git.hasERBranch = 
+/**
+ * Checks if the ERScaffold branch is created or not
+ * @returns {Promise} will be resolved with bool
+ */
+function() {
+    return new Promise((resolve,reject) => {
+        sgit.branchLocal()
+        .then(summary => {
+            let interest = Object.keys(summary.branches).filter(name => name.indexOf(ERSCAFFOLD_BRANCH_NAME) != -1)
+            resolve(interest.length == 1)
+        }).catch(err => {
+            resolve(false)
+        })
+    })
+}
+
+git.createAndCheckoutERBranch =
+
+/**
+ * Creates ERBranch from the current branch 
+ * @returns {Promise}
+ */
+function() {
+    return sgit.checkout(['-b', ERSCAFFOLD_BRANCH_NAME])
+}
+
+git.checkoutERBranch = 
+
+/**
+ * checkout to ERBranch
+ * @returns {Promise}
+ */
+function() {
+    return sgit.checkout([ERSCAFFOLD_BRANCH_NAME]) 
+}
+
+git.commit =
+
+/**
+ * Commits modifications
+ * @returns {Promise}
+ */
+function() {
+    return sgit.commit(COMMIT_MESSAGE,'.')
+}
+
+git.rollbackToFirstCommit =
+
+/**
+ * Rolls back to the first commit
+ * @returns {Promise}
+ */
+function() {
+    return sgit
+    .log(['--first-parent'])
+    .then(summary => {
+        let lastHash = summary.all[summary.length-1].hash
+        return sgit.revert(lastHash)
+    })
+}
