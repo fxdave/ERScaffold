dffe03a0ac24561ec71c1ff24fc5b7d06dc627d4
David Judge <biro97@gmail.com>
2019-02-26 00:23:28 +0100

    formatted

--- app/main/PackHandler/PackHandler.js
+++ app/main/PackHandler/PackHandler.js
@@ -1,59 +1,59 @@
-import EntityPacksPair from './EntityPacksPair';
-import PackReader from './PackReader/PackReader';
-import TemplateReader from './TemplateReader/TemplateReader';
-import Generator from './Generator/Generator';
+import EntityPacksPair from './EntityPacksPair'
+import PackReader from './PackReader/PackReader'
+import TemplateReader from './TemplateReader/TemplateReader'
+import Generator from './Generator/Generator'
 
 class PackHandler {
-  /**
-   *
-   * @param {FsWrapper} fsWrapper
-   * @param {GitWrapper}
-   */
-  constructor(fsWrapper, gitWrapper) {
-    this.packReader = new PackReader(fsWrapper);
-    this.templateReader = new TemplateReader(fsWrapper);
-    this.generator = new Generator(fsWrapper, gitWrapper);
-  }
-
-  /**
-   *
-   * @param {Pack[]} packs
-   * @param {Entity} entiy
-   * @returns {Pack[]} with the templates with a "disabled" boolean member
-   */
-  _testPacksForEntity(packs, entiy) {
-    return packs.map(pack => {
-      pack.requirement = pack.requirements.map(req => {
-        req.disabled = false;
-        if (req.data(entiy) === null) req.disabled = true;
-
-        return req;
-      });
-
-      return pack;
-    });
-  }
-
-  /**
-   *
-   * @async
-   * @param {Entity[]} entities
-   * @returns {EntityPacksPair[]}
-   */
-  async getOptionsForEntities(entities) {
-    const options = {};
-
-    let packs = await this.packReader.getPacks();
-
-    entities.forEach(entity => {
-      options[entity.getID()] = new EntityPacksPair(
-        entity,
-        this._testPacksForEntity(packs, entity)
-      );
-    });
-
-    return Object.values(options);
-  }
+    /**
+     *
+     * @param {FsWrapper} fsWrapper
+     * @param {GitWrapper}
+     */
+    constructor(fsWrapper, gitWrapper) {
+        this.packReader = new PackReader(fsWrapper)
+        this.templateReader = new TemplateReader(fsWrapper)
+        this.generator = new Generator(fsWrapper, gitWrapper)
+    }
+  
+    /**
+     *
+     * @param {Pack[]} packs
+     * @param {Entity} entiy
+     * @returns {Pack[]} with the templates with a "disabled" boolean member
+     */
+    _testPacksForEntity(packs, entiy) {
+        return packs.map(pack => {
+            pack.requirement = pack.requirements.map(req => {
+                req.disabled = false
+                if (req.data(entiy) === null) req.disabled = true
+        
+                return req
+            })
+      
+            return pack
+        })
+    }
+  
+    /**
+     *
+     * @async
+     * @param {Entity[]} entities
+     * @returns {EntityPacksPair[]}
+     */
+    async getOptionsForEntities(entities) {
+        const options = {}
+    
+        let packs = await this.packReader.getPacks()
+    
+        entities.forEach(entity => {
+            options[entity.getID()] = new EntityPacksPair(
+                entity,
+                this._testPacksForEntity(packs, entity)
+            )
+        })
+    
+        return Object.values(options)
+    }
 }
 
-export default PackHandler;
+export default PackHandler
