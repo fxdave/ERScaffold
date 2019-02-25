5a0091d1ef95739f4bf22ef334966c91352f116f
David Judge <biro97@gmail.com>
2019-02-26 00:22:41 +0100

    formatted

--- app/main/PackHandler/EntityPacksPair.js
+++ app/main/PackHandler/EntityPacksPair.js
@@ -1,12 +1,12 @@
 class EntityPacksPair {
-  /**
-   * @param {Entity} entity
-   * @param {Pack[]} packs
-   */
-  constructor(entity, packs) {
-    this.entity = entity;
-    this.packs = packs;
-  }
+    /**
+     * @param {Entity} entity
+     * @param {Pack[]} packs
+     */
+    constructor(entity, packs) {
+        this.entity = entity
+        this.packs = packs
+    }
 }
 
-export default EntityPacksPair;
+export default EntityPacksPair
