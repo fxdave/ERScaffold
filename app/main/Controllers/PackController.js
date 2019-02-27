2cafa2d546a1e520fd3c6e1ccb1b5d519465e921
David Judge <biro97@gmail.com>
2019-02-27 22:46:27 +0100

    modified PackController

--- app/main/Controllers/PackController.js
+++ app/main/Controllers/PackController.js
@@ -1,76 +1,112 @@
-import Controller from './Controller';
-import Model from '../Model/Model';
+import os from 'os'
+import Controller from './Controller'
+import ERModel from '../Model/ERModel'
+import PackCollection from '../Model/PackCollection'
+import Pack from '../Model/Pack'
+import PackCollectionReader from '../PackUtils/PackCollectionReader/PackCollectionReader'
+import TemplateRenderer from '../PackUtils/TemplateRenderer/TemplateRenderer'
+import Generator from '../PackUtils/Generator/Generator'
 
 class PackageListItem {
-  constructor(id, name, packs) {
-    this.entity = {
-      id,
-      name
-    };
+    constructor(id, name, packs) {
+        this.entity = {
+            id,
+            name
+        }
 
-    this.packs = packs;
-  }
+        this.packs = packs
+    }
 }
 
 class PackController extends Controller {
-  /**
-   *
-   * @param {PackHandler} packHandler
-   */
-  constructor(packHandler) {
-    super();
-    this.model = null;
-    this.packHandler = packHandler;
-  }
-
-  /**
-   * @async
-   * @param {Object} e
-   * @param {Object} model the ERModel
-   * @returns {PackageListItem[]|...{success, msg}}
-   */
-  async listPackages(e, model) {
-    try {
-      this.model = new Model(model);
-
-      let options = await this.packHandler.getOptionsForEntities(
-        this.model.getEntities()
-      );
-
-      return options.map(
-        ({ entity, packs }) =>
-          new PackageListItem(entity.id, entity.name, packs)
-      );
-    } catch (error) {
-      return {
-        success: false,
-        msg: "Sorry couldn't fetch the pack, details:" + error
-      };
+    /**
+     *
+     * @param {PackCollectionReader} packCollectionReader
+     * @param {TemplateRenderer} templateRenderer
+     * @param {Generator} generator
+     */
+    constructor(packCollectionReader, templateRenderer, generator) {
+        super()
+        this.model = null
+        this.packCollectionReader = packCollectionReader
+        this.templateRenderer = templateRenderer
+        this.generator = generator
     }
-  }
 
-  /**
-   *
-   * @param {Object} e
-   * @param {Object[]} data the selected templates
-   */
-  async generateSelectedPackages(e, data) {
-    this.model.getEntities().forEach(async entity => {
-      let templates = await Promise.all(
-        data.map(template =>
-          this.packHandler.templateReader.getTemplate(
-            template.pack,
-            template.template,
-            { entity }
-          )
-        )
-      );
+    /**
+     * @asnyc
+     * @returns {Pack[]}
+     */
+    async _getUserPacks() {
+        /*
+        try {
+            let packsFolderUser = `${os.homedir()}/.config/erscaffold/packs`
+            return await this.packCollectionReader.getPacks(packsFolderUser)
+        } catch {
+            return []
+        }
+        */
+        return [] // TODO: implement it well 
+    }
 
-      this.packHandler.generate(templates);
+    /**
+     * @async
+     * @param {Object} e
+     * @param {Object} model the ERModel
+     * @returns {PackageListItem[]|...{success, msg}}
+     */
+    async listPackages(e, model) {
+        try {
+            this.model = new ERModel(model)
 
-      return { succcess: true, msg: 'Sorry this is not implemented yet' };
-    });
-  }
+            // getting builtin packs
+            let packsFolderBuiltin = path.join(__dirname, '../../../packs')
+            let packsBuiltin = await this.packCollectionReader.getPacks(packsFolderBuiltIn)
+
+            // getting user packs
+            let packsUser = await this._getUserPacks()
+            
+            // get all packs together
+            let packs = new PackCollection(...packsBuiltin,...packsUser)
+
+            // and filter out which requirement is allowed for that model
+            let options = packs.getOptionsForEntities(this.model.getEntities())
+
+            return options.map(
+                ({ entity, packs }) =>
+                    new PackageListItem(entity.id, entity.name, packs)
+            )
+        } catch (error) {
+            return {
+                success: false,
+                msg: 'Sorry couldn\'t fetch the pack, details:' + error
+            }
+        }
+    }
+
+    /**
+     *
+     * @param {Object} e
+     * @param {Object[]} data the selected templates
+     * @param {string} data[].pack the name of the packfolder
+     * @param {string} data[].template the path of the templatefile
+     */
+    async generateSelectedPackages(e, data) {
+        this.model.getEntities().forEach(async entity => {
+            let templates = await Promise.all(
+                data.map(template =>
+                    this.templateRenderer.renderTemplate(
+                        path.join(template.pack, template.template),
+                        { entity }
+                    )
+                )
+            )
+
+            this.packHandler.generate(templates)
+
+            return { succcess: true, msg: 'Sorry this is not implemented yet' }
+        })
+    }
 }
 
-export default PackController;
+export default PackController
